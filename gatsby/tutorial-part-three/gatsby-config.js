30395bd4712db860f7dc624a1700f4edfa92dd2b
usyyy <usmaanalii@hotmail.co.uk>
2017-08-05 20:07:36 +0100

    - Completed Part 3 of the official Tutorial   - Layout components

---
+++ gatsby/tutorial-part-three/gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-typography',
+      pathToConfigModule: 'src/utils/typography',
+    },
+  ],
+};
---
+++ gatsby/tutorial-part-three/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () =>
+  <div>
+    <h1>Hi! I'm building a fake Gatsby sire as part of a tutorial</h1>
+    <p>
+      What do i like to do? Lots of course, but definitely enjoy building websites.
+    </p>
+  </div>
