5fbf361d46f9f307f9d8a993f8298c73a271e624
usyyy <usmaanalii@hotmail.co.uk>
2017-08-29 00:19:00 +0100

    Playing around with rough draft for plotly chart

--- seaborn_to_dash_conversion/other/linear_regression.py
+++ seaborn_to_dash_conversion/other/linear_regression.py
@@ -1,5 +1,6 @@
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.linregress.html
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
@@ -18,8 +19,7 @@ x_spin_vals = y_spin_vals.index
 pace_slope, pace_intercept, pace_r_value, pace_p_value, pace_std_err = stats.linregress(
     x_pace_vals, y_pace_vals)
 
-spin_slope, spin_intercept, spin_r_value, spin_p_value, spin_std_err = stats.linregress(
-    x_spin_vals, y_spin_vals)
+spin_slope, spin_intercept, spin_r_value, spin_p_value, spin_std_err = stats.linregress(x_spin_vals, y_spin_vals)
 
 plt.plot(x_pace_vals, y_pace_vals, 'o', label='pace')
 plt.plot(x_pace_vals, pace_intercept + pace_slope * x_pace_vals, 'r')
